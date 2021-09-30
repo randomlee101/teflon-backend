98bccbf021092ee53ec4b9d3c4945836f7e85119
danielswipe <daniel@swipe.ng>
2021-09-30 15:38:25 +0100

    front-end cors

--- src/main/java/com/randomlee/teflonbackend/security_config/CorsConfig.java
+++ src/main/java/com/randomlee/teflonbackend/security_config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200");
+                        .allowedOrigins("http://localhost:4200","https://youthful-wozniak-c9e54c.netlify.app");
             }
         };
     }
